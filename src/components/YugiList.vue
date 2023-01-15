2d5162930796f164f118f0e095a6dd7bf92fe61a
mattiabenelli <benellimattia0@gmail.com>
2023-01-15 21:55:03 +0100

    es done

--- src/components/YugiList.vue
+++ src/components/YugiList.vue
@@ -15,17 +15,26 @@ export default {
 </script>
 
 <template lang="">
-    <div class="container-cards">
-        <div class="row-table">
-            <YugiCard v-for="(item,index) in store.YugiList" :key="index" :card="item" />
+    <div class="border-container">
+        <div class="container-cards">
+            <div class="card-counter"></div>
+            <div class="row-table">
+                <YugiCard v-for="(item,index) in store.YugiList" :key="index" :card="item"/>
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss">
     .container-cards{
+        width: 95%;
+        margin: 0 auto;
+    }
+    .border-container{
+        background-color: white;
         width: 80%;
-        margin: 0 auto
+        margin: 0 auto;
+        padding: 20px 0;
     }
   
 </style>
