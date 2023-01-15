5bca73958fbcb6991e70c4fd1be793195479bcb1
mattiabenelli <benellimattia0@gmail.com>
2023-01-15 18:35:11 +0100

    immagini stampate

--- src/components/YugiCard.vue
+++ src/components/YugiCard.vue
@@ -7,11 +7,25 @@ export default {
 </script>
 
 <template lang="">
-  <div>
-    <div>{{ card.name }}</div>
-  </div>
+    <div class="card">
+        <div><img :src="card.card_images[0].image_url" alt=""></div>
+        <div class="card-description">
+            <div>{{ card.name }}</div>
+            <div>{{ card.archetype }}</div>
+        </div>
+    </div>
 </template>
 
-<style lang="">
-  
+<style lang="scss">
+  .card{
+    width: calc(100% / 5);
+    background-color: orange;
+        img{
+            width: 100%;
+        }
+  }
+  .card-description{
+    text-align: center;
+    padding: 10px 0;
+  }
 </style>
--- src/components/YugiList.vue
+++ src/components/YugiList.vue
@@ -15,11 +15,17 @@ export default {
 </script>
 
 <template lang="">
-  <div>
-    <YugiCard v-for="(item,index) in store.YugiList" :key="index" :card="item" />
-  </div>
+    <div class="container-cards">
+        <div class="row-table">
+            <YugiCard v-for="(item,index) in store.YugiList" :key="index" :card="item" />
+        </div>
+    </div>
 </template>
 
-<style lang="">
+<style lang="scss">
+    .container-cards{
+        width: 80%;
+        margin: 0 auto
+    }
   
 </style>
